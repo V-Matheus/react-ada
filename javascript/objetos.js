f83403f39040e7612349ff060ef23fb2b9a228a9
V-Matheus <victormatheus507@gmail.com>
2024-03-13 20:01:40 -0300

    objetos 2

--- javascript/objetos.js
+++ javascript/objetos.js
@@ -3,3 +3,23 @@
 const array = ['Matheus', 19, 1.80, true]
 
 const pessoa = {nome: 'Matheus', anos: 19, altura: 1.80, matriculado: true}
+
+console.log(pessoa);
+
+pessoa.nome = 'victor matheus'
+
+delete pessoa.altura
+
+const idade = 19
+const altura = 1.80
+
+console.clear()
+
+const objeto = {
+  idade: idade,
+  altura: altura,
+}
+
+console.log(objeto);
+
+const {nome} = pessoa
