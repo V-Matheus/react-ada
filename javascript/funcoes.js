b51d1ca272940d8a55436750d3a0b22b0fb59208
V-Matheus <victormatheus507@gmail.com>
2024-03-13 20:21:34 -0300

    funcoes de alta ordem

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -10,12 +10,12 @@ console.log(mensagemSaudacao);
 console.clear()
 
 // Exemplo 2
-function somar(num1, num2) {
-  return num1 + num2
-}
+// function somar(num1, num2) {
+//   return num1 + num2
+// }
 
-const resultado = somar(2,3)
-console.log(resultado);
+// const resultado = somar(2,3)
+// console.log(resultado);
 
 // Funções anônimas
 
@@ -28,8 +28,43 @@ console.log(dobro);
 
 // Arrow functions
 
-const subtrair = (num1, num2) => {
-  return num1 - num2
+// const subtrair = (num1, num2) => {
+//   return num1 - num2
+// }
+
+// const multiplicar = (num1, num2) => num1 * num2 
+
+console.clear()
+
+// funcoes de alta ordem
+
+// 1º Função que retorna outra função
+
+function welcome(courseName ) {
+  return (estutantsName) => {
+    console.log(`Ola ${estutantsName}, bem vindo ao curso de ${courseName}`);
+  }
 }
 
-const multiplicar = (num1, num2) => num1 * num2 
+const welcomeToFrontEndCorse = welcome('frontEnd em React')
+
+welcomeToFrontEndCorse('Matheus')
+
+console.clear()
+
+// 2º Funções que recebe outra função como parâmetro
+
+const somar = (num1, num2) => num1 + num2
+const subtrair = (num1, num2) => num1 - num2
+const multiplicar = (num1, num2) => num1 * num2
+const dividir = (num1, num2) => num1 / num2
+
+const calcular = (num1, num2, operacao) => operacao(num1, num2)
+
+const retorno = calcular(10, 5, multiplicar)
+
+console.log(retorno);
+
+const resultado = calcular(10, 5, (num1, num2) => (num1 * num2) + (2 * num1 * num2))
+
+console.log(resultado);
