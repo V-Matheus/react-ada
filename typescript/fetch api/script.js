5ff32c50d3bd32d434c5ca249779ee0f4e89d12e
V-Matheus <victormatheus507@gmail.com>
2024-03-19 14:54:44 -0300

    consumindo api com async e await

--- typescript/fetch api/script.js
+++ typescript/fetch api/script.js
@@ -1,10 +1,25 @@
 // Fetch api
 
 // then e catch
-fetch('https://viacep.com.br/ws/01001000/json/')
-  .then((response) => {
-    response.json().then((dados) => console.log(dados));
-  })
-  .catch((err) => {
+// fetch('https://viacep.com.br/ws/01001000/json/')
+//   .then((response) => {
+//     response.json().then((dados) => console.log(dados));
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// Async e await
+
+async function obterDadosDoCep() {
+  try {
+    const response = await fetch('https://viacep.com.br/ws/01001000/json/');
+    const dados = await response.json();
+
+    console.log(dados);
+  } catch (err) {
     console.log(err);
-  });
+  }
+}
+
+obterDadosDoCep();
