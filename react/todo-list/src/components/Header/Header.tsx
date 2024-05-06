396b48bde1652272f3aafb9b246d1da38f14e676
Victor Matheus <victormatheus507@gmail.com>
2024-05-06 13:42:48 -0300

    first styles

--- react/todo-list/src/components/Header/Header.tsx
+++ react/todo-list/src/components/Header/Header.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from 'react';
+import styles from './styles.module.scss';
+import StatsCard from '../StatsCard/StatsCard';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <div>
-        <h1>My Todo</h1>
+      <div className={styles.container}>
+        <div>
+          <h1>My Todo</h1>
 
-        <span>Bem-vindo, Matheus</span>
+          <span>Bem-vindo, Matheus</span>
+        </div>
+
+        <div>
+          <StatsCard title='Total de tarefas' value={5}/>
+          <StatsCard title='Tarefas pendentes' value={4}/>
+          <StatsCard title='Tarefas concluidas' value={1}/>
+        </div>
       </div>
-
-      <div></div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
