424d80ba2998b8b2b8f7e0b7e0a75a387ac1a3de
Victor Matheus <victormatheus507@gmail.com>
2024-05-06 09:54:09 -0300

    refatorando o código

--- react/primeiro-projeto/src/components/Article/Article.jsx
+++ react/primeiro-projeto/src/components/Article/Article.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import './styles.css'
+import './styles.css';
 
-export class Article extends React.Component {
-  render() {
-    return (
-      <article>
-        <img src="" alt="" />
-        <div className="article-infos">
-          <h2>{this.props.title}</h2>
-          <h3>{this.props.provider}</h3>
+export function Article({title, provider, description}) {
+  return (
+    <article>
+      <img src="" alt="" />
+      <div className="article-infos">
+        <h2>{title}</h2>
+        <h3>{provider}</h3>
 
-          <p>
-          {this.props.description}
-          </p>
-        </div>
-      </article>
-    );
-  }
+        <p>{description}</p>
+      </div>
+    </article>
+  );
 }
-
-
