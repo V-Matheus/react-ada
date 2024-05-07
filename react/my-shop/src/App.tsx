12de6f589302febced819af6cc6de0a8bca74719
Victor Matheus <victormatheus507@gmail.com>
2024-05-07 16:14:30 -0300

    estilos globais

---
+++ react/my-shop/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+import { GlobalStyles } from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
+}
+export default App;
